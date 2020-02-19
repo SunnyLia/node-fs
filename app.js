56b1eeaba288c5ca99a1a79d557d2a2f83ada4dc
sunnyLia <1326155622@qq.com>
2020-02-19 18:33:27 +0800

    新增代码

--- app.js
+++ app.js
@@ -1,37 +1,13 @@
 const express = require("express");
 const app = express();
-const fs = require("fs");
-const path = require("path");
+
 const com = require("./views/static/common/common");
 
 app.use(express.static('./views'))
 
 app.set("view engine", "ejs");
 
-app.all('*', function (req, res) {
-    var path1 = decodeURI(req.path);
-    fs.readdir("./views/static" + path1, function (err, files) {
-        const allFold = [];
-        if (err) {
-            res.render("error")
-            return
-        }
-        files.forEach(function (file) {
-            let stats = fs.statSync("./views/static" + path1 + file);
-            allFold.push({
-                isFold: stats.isDirectory(),
-                foldName: file,
-                type: path.extname(file).substr(1).toUpperCase(),
-                dir: path1 + file,
-                date: com.dateFormat(stats.atime),
-                size: (stats.size / 1024).toFixed(2)
-            })
-        })
-        // res.send({ data: allFold })
-        res.render("index", { foldLists: allFold, breadcrumbs: path1.split("/"), title: 1 })
-    })
-
-});
+app.all('*',com.readdir);
 
 app.listen("8081", () => {
     console.log(`server run in 8081`)
--- views/static/common/common.js
+++ views/static/common/common.js
@@ -1,4 +1,46 @@
 const moment = require("moment");
-exports.dateFormat = function(date){
+const fs = require("fs");
+const path = require("path");
+
+exports.readdir = function (req, res) {
+    var path1 = decodeURI(req.path);
+    fs.readdir("./views/static" + path1, function (err, files) {
+        const allFold = [];
+        if (err) {
+            res.render("error")
+            return
+        }
+        files.forEach(function (file) {
+            let stats = fs.statSync("./views/static" + path1 + file);
+            allFold.push({
+                isFold: stats.isDirectory(),
+                foldName: file,
+                type: path.extname(file).substr(1).toUpperCase(),
+                dir: path1 + file,
+                date: dateFormat(stats.atime),
+                size: (stats.size / 1024).toFixed(2)
+            })
+        })
+        // res.send({ data: allFold })
+        res.render("index", { foldLists: allFold, breadcrumbs: path1.split("/"), title: callback })
+    })
+}
+
+const dateFormat = function (date) {
     return moment(date).format('YYYY/MM/DD hh:mm');
+}
+
+const copyFile = function () {
+
+}
+
+const callback = function (tex, path) {
+    console.log(tex, path);
+    if (tex == '打开') {
+    } else if (tex == '复制') {
+    } else if (tex == '粘贴') {
+    } else if (tex == '剪切') {
+    } else if (tex == '删除') {
+    } else if (tex == '重命名') {
+    }
 }
