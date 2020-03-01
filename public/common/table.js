e82190648e2791d9762ad94bb8aa82d8487cf1aa
sunnyLia <1326155622@qq.com>
2020-03-01 19:44:43 +0800

    table页面该ui框架

--- public/common/table.js
+++ public/common/table.js
@@ -1,44 +1,60 @@
 
-var pageSize = 10, page = 1;
-layui.use(['form', 'layedit', 'laydate', 'table'], function () {
+var pageSize = 10, page = 1, modal = 1;
+layui.use(['form', 'layedit', 'layer', 'laydate', 'table'], function () {
     var form = layui.form
         , layer = layui.layer
         , layedit = layui.layedit
         , laydate = layui.laydate
         , table = layui.table;
-    table.render({
-        elem: '#formList'
-        , url: '/demo/table/user/' //数据接口
-        , page: true //开启分页
+
+    var tableIns = table.render({
+        elem: '#tableList'
+        , url: '/query'
+        , page: true
         , toolbar: '#toolbarDemo'
+        , where: { data: JSON.stringify({}) }
         , defaultToolbar: []
-        , cols: [[ //表头
-            { field: 'id', title: '序号', width: 80, fixed: 'left' }
-            , { field: 'author', title: '标题', width: 80 }
-            , { field: 'title', title: '作者', width: 80 }
-            , { field: 'dynasty', title: '朝代', width: 80 }
-            , { field: 'content', title: '内容', width: 177 }
-            , { field: 'type', title: '分类', width: 80 }
+        , cols: [[
+            { field: 'id', type: 'numbers', title: '序号', fixed: 'left' }
+            , { field: 'author', title: '标题' }
+            , { field: 'title', title: '作者' }
+            , { field: 'dynasty', title: '朝代' }
+            , { field: 'content', title: '内容' }
+            , { field: 'type', title: '分类' }
         ]]
         , page: true
     });
+    form.on('submit(formList)', function (data) {
+        console.log(data.field);
+        return false;
+    });
+    form.on('submit(modalForm)', function (data) {
+        console.log(data.field);
+        $.get("/addPoetry", { data: JSON.stringify(data.field) }, function (data) {
+            if (data.code == 0) {
+                layer.close(modal);
+                layer.msg('更新成功 ！');
+                tableIns.reload();
+            } else {
+                layer.msg('请稍后重试！');
+            }
+        })
+        return false;
+    });
     //监听事件
-    table.on('toolbar(formList)', function (obj) {
+    table.on('toolbar(tableList)', function (obj) {
         var checkStatus = table.checkStatus(obj.config.id);
+        console.log(checkStatus);
+
         switch (obj.event) {
             case 'add':
-                // layer.confirm($('#modalDemo'), { title: '提示' }, function (index) {
-                //     layer.close(index);
-                // });
                 layer.open({
                     type: 1,
                     title: "新增",
-                    btn: ['确认', '取消'],
                     btnAlign: 'c',
                     content: $("#modalDemo"),
-                    success: function (layero, index) { },
-                    yes: function () {
-
+                    success: function (layero, index) {
+                        modal = index;
                     }
                 });
                 break;
@@ -51,38 +67,3 @@ layui.use(['form', 'layedit', 'laydate', 'table'], function () {
         };
     });
 })
-// var form_data = $("#formList").serializeArray();
-// var result = {};
-// form_data.forEach(ele => {
-//     if (ele.name == "type" && ele.value == "0") ele.value = "";
-//     if (ele.value) result[ele.name] = ele.value;
-// });
-
-
-// $.get("/query", { data: JSON.stringify(result), pageSize: pageSize, page: page }, function (data) {
-//     if (data.code != 1) {
-//         $("#noData").show()
-//         $("#pagination").hide()
-//     } else {
-//         $("#noData").hide()
-//         $("#pagination").show()
-//         showTable(data.data)
-//     }
-// })
-
-
-// function addPoetry() {
-//     var form_data = $("#mondalForm").serializeArray();
-//     var result = {};
-//     form_data.forEach(ele => {
-//         if (ele.name == "type" && ele.value == "0") ele.value = "";
-//         if (ele.value) result[ele.name] = ele.value;
-//     });
-//     if ($.isEmptyObject(result) == true) {
-//         alert("没有输入信息，不能提交哦~")
-//     } else {
-//         $.get("/addPoetry", { data: JSON.stringify(result) }, function (data) {
-
-//         })
-//     }
-// }
--- routers/table.js
+++ routers/table.js
@@ -6,22 +6,36 @@ exports.poetry = function (req, res) {
 }
 exports.query = function (req, res) {
     var data = JSON.parse(req.query.data);
-    var size = parseInt(req.query.pageSize);
+    var size = parseInt(req.query.limit);
     var skip = (req.query.page - 1)*size;
     
-    // MongoClient.connect(url, { useUnifiedTopology: true }, function (err, client) {
-    //     if (err) res.send({code:0,msg:err});
-    //     var db = client.db("user"); //获取到user数据库
-    //     var col = db.collection("poetry"); //获取到student集合(表)
-    //     col.find(data).skip(skip).limit(size).toArray((err, docs) => {
-    //         console.log(docs);
-    //         if(docs){
-    //             res.send({data:docs,code:1})
-    //         }else{
-    //             res.send({data:[],code:0})
-    //         }
-    //     });
-    //     client.close();
-    // });
+    MongoClient.connect(url, { useUnifiedTopology: true }, function (err, client) {
+        if (err) res.send({code:1,msg:err});
+        var db = client.db("user"); //获取到user数据库
+        var col = db.collection("poetry"); //获取到student集合(表)
+        col.find(data).skip(skip).limit(size).toArray((err, docs) => {
+            if (err) {
+                res.send({code:1,msg:err});
+            }else{
+                res.send({code:0,data:docs})
+            }
+            client.close();
+        });
+    });
 }
-exports.addPoetry = function (req, res) {}
+exports.addPoetry = function (req, res) {
+    var data = JSON.parse(req.query.data);
+    MongoClient.connect(url, { useUnifiedTopology: true }, function (err, client) {
+        if (err) res.send({code:1,msg:err});
+        var db = client.db("user"); //获取到user数据库
+        var col = db.collection("poetry"); //获取到student集合(表)
+        col.insertOne(data,function(err,result){
+            if (err) {
+                res.send({code:1,msg:err});
+            }else{
+                res.send({code:0})
+            }
+            client.close();
+        })
+    });
+}
