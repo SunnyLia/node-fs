0584ead367ab7f79799953f4c534359ce0a4eb87
sunnyLia <1326155622@qq.com>
2020-03-05 18:39:47 +0800

    修改查询代码

--- public/common/table.js
+++ public/common/table.js
@@ -3,38 +3,29 @@
 layui.use(['form', 'layedit', 'layer', 'laydate', 'table'], function () {
     var form = layui.form
         , layer = layui.layer
-        , layedit = layui.layedit
         , laydate = layui.laydate
         , table = layui.table;
-
-    var tableIns = table.render({
-        elem: '#tableList'
-        , url: '/query'
-        , page: true
-        , even: true
-        , toolbar: '#toolbarDemo'
-        , where: { data: JSON.stringify({}) }
-        , defaultToolbar: []
-        , cols: [[
-            { type: 'checkbox', fixed: 'left' }
-            , { field: 'id', type: 'numbers', align: 'center', title: '序号', fixed: 'left' }
-            , { field: 'author', align: 'center', title: '作者' }
-            , { field: 'title', align: 'center', title: '标题' }
-            , { field: 'dynasty', align: 'center', title: '朝代' }
-            , { field: 'content', align: 'center', title: '内容' }
-            , { field: 'type', align: 'center', title: '分类' ,templet: type_came}
-        ]]
-        , page: true
-    });
+    var tableIns="";
     // 表单查询
     form.on('submit(formList)', function (data) {
-        console.log(data.field);
+        var queryData = $('#formList').serializeArray().reduce(function (obj, item) {
+            if (item.value) {
+                obj[item.name] = item.value;
+                return obj;
+            }
+            return obj;
+        }, {});
+        query(queryData) 
         return false;
     });
     // 弹框提交
     form.on('submit(modalForm)', function (data) {
-        var checkStatus = table.checkStatus("tableList").data[0];
-        $.get("/addPoetry", { data: JSON.stringify(data.field), _id: checkStatus._id }, function (data) {
+        if ($("#_id").val()) {
+            var url = "/editPoetry"
+        } else {
+            var url = "/addPoetry";
+        }
+        $.get(url, { data: JSON.stringify(data.field), _id: $("#_id").val() }, function (data) {
             if (data.code == 0) {
                 layer.closeAll();
                 tableIns.reload();
@@ -56,9 +47,8 @@ layui.use(['form', 'layedit', 'layer', 'laydate', 'table'], function () {
                     btnAlign: 'c',
                     content: $("#modalDemo"),
                     success: function (layero, index) {
-                        console.log(layero);
-                        
                         $("#modalForm")[0].reset();
+                        $("#_id").val("");
                     }
                 });
                 break;
@@ -66,13 +56,13 @@ layui.use(['form', 'layedit', 'layer', 'laydate', 'table'], function () {
                 if (checkStatus.length == 1) {
                     layer.confirm('确认要删除这条数据嘛?', { title: '提示' }, function (index) {
                         $.get("/delPoetry", { _id: checkStatus[0]._id }, function (data) {
+                            layer.close(index);
                             if (data.code == 0) {
                                 tableIns.reload();
                                 layer.msg('删除成功 ！');
                             } else {
                                 layer.msg('请稍后重试！');
                             }
-                            layer.closeAll();
                         })
                     });
                 } else {
@@ -87,6 +77,7 @@ layui.use(['form', 'layedit', 'layer', 'laydate', 'table'], function () {
                         btnAlign: 'c',
                         content: $("#modalDemo"),
                         success: function (layero, index) {
+                            $("#_id").val(checkStatus[0]._id);
                             form.val("modalForm", checkStatus[0]);
                         }
                     });
@@ -96,15 +87,36 @@ layui.use(['form', 'layedit', 'layer', 'laydate', 'table'], function () {
                 break;
         };
     });
-    function type_came(v){
-        if(v.type == 1){
+    function query(data) {
+        tableIns = table.render({
+            elem: '#tableList'
+            , url: '/query'
+            , page: true
+            , even: true
+            , toolbar: '#toolbarDemo'
+            , where: { data: JSON.stringify(data) }
+            , defaultToolbar: []
+            , cols: [[
+                { type: 'checkbox', fixed: 'left' }
+                , { field: 'id', type: 'numbers', align: 'center', title: '序号', fixed: 'left' }
+                , { field: 'author', align: 'center', title: '作者' }
+                , { field: 'title', align: 'center', title: '标题' }
+                , { field: 'dynasty', align: 'center', title: '朝代' }
+                , { field: 'content', align: 'center', title: '内容' }
+                , { field: 'type', align: 'center', title: '分类', templet: type_came }
+            ]]
+            , page: true
+        });
+    }
+    function type_came(v) {
+        if (v.type == 1) {
             return "写事写人"
-        }else if(v.type == 2){
+        } else if (v.type == 2) {
             return "写景咏物"
-        }else if(v.type == 3){
+        } else if (v.type == 3) {
             return "言志抒情"
-        }else{
-            return "其他"
+        } else {
+            return ""
         }
     }
 })
