1ac458e0a33b092f3eafc64bad1522b263dd4047
sunnyLia <1326155622@qq.com>
2020-03-18 22:56:39 +0800

    新增socket传输

--- routers/apiSocket.js
+++ routers/apiSocket.js
@@ -1,30 +1,37 @@
+const moment = require("moment");
 var mongodb = require("mongodb");
 var MongoClient = mongodb.MongoClient;
 var url = "mongodb://localhost:27017/";
 
 exports.socket = function (socket) {
-
     socket.on('join', function (info) {
-
         MongoClient.connect(url, { useUnifiedTopology: true }, function (err, client) {
             if (err) {
-                console.log(err);
                 socket.emit('chatLists', { code: 1, msg: err });
             }
             var db = client.db("user");
             var col = db.collection("chatLists"); //获取到chatLists表
-            
             col.find({ "roomId": info.id }).toArray((err, docs) => { //获取到所有的文档
                 if (err) {
-                    console.log(err);
                     socket.emit('chatLists', { code: 1, msg: err });
                 } else {
-                    console.log(33333333333);
                     socket.emit('chatLists', { code: 0, data: docs });
-                    console.log(444444444444);
                 }
                 client.close();
             })
         });
     })
+    socket.on('sendMsg', function (info) {
+        var docs = [{
+            userName: info.userName,
+            userId: info.userId || (new Date()).getTime(),
+            userChat: info.msg,
+            userChatTime: dateFormat(new Date()),
+            roomId: info.roomId
+        }]
+        socket.emit('chatLists', { code: 0, data: docs });
+    })
+}
+const dateFormat = function (date) {
+    return moment(date).format('YYYY/MM/DD HH:mm:ss');
 }
