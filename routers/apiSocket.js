28c9a583c2a67b0756d44fcd66012e2edd5a4614
sunnyLia <1326155622@qq.com>
2020-03-21 17:44:07 +0800

    修改房间在线人数统计bug

--- routers/apiSocket.js
+++ routers/apiSocket.js
@@ -6,19 +6,22 @@ exports.socket1 = function (io) {
     io.on('connection', function (socket) {
         var info1 = {};
         socket.on('join', function (info, fn) {
-            info1 = info;
+            info1["roomId"] = info.id;
             socket.join(info.id); //添加到房间
-            var count = io.eio.clientsCount;  //当前链接数
-            fn(count);//客户端回调
 
-            socket.broadcast.to(info.id).emit('inORout', { msg: info.user + "进入群聊", count: count });//广播给房间内其他人
+            if (socket.adapter.rooms[info.id]) {
+                info1["count"] = socket.adapter.rooms[info.id].length;
+            }
+
+            fn(info1.count);//客户端回调
+
+            socket.broadcast.to(info.id).emit('inORout', { msg: info.user + "进入群聊", count: info1.count });//广播给房间内其他人
             io.to(info.id).emit('chatLists', {
                 code: 0, data: [{ "userName": "机器喵", "userChat": "欢迎" + info.user + "童鞋加入群聊~" }]
             });//广播给房间所有人
         })
         socket.on('inORout', function (data) { //用户退出监测
-            var count = io.eio.clientsCount - 1;  //当前链接数
-            socket.broadcast.to(info1.id).emit('inORout', { msg: data + "退出群聊", count: count });
+            socket.broadcast.to(info1.roomId).emit('inORout', { msg: data + "退出群聊", count: info1.count - 1 });
         });
         socket.on('sendMsg', function (info) {
             var docs = [{
