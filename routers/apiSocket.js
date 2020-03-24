909a2db64ee4aaa9928016df7570ec464d9c8ca2
sunnyLia <1326155622@qq.com>
2020-03-24 19:01:01 +0800

    当前群聊在线人数跟sendMsg需要再看下，有bug

--- routers/apiSocket.js
+++ routers/apiSocket.js
@@ -6,98 +6,84 @@ var userList = {};
 exports.socket1 = function (io) {
     io.on('connection', function (socket) {
         // console.log("用户上线");
-        var info1 = {}; //roomId,user,count
+        var info1 = {}; //保存roomId,user,count
+        /* 登陆 login */
+        socket.on('login', function (info) {
+            userList[info.user] = socket.id; //把用户socketid存到以用户为属性的userList对象里
+        })
+        /* 加入群聊 join */
         socket.on('join', function (info, fn) {
             info1 = info;
-            // userList[info.user] = socket.id; //把用户socketid存到以用户为属性的list对象里
             socket.join(info.roomId); //添加到房间
-            if (info.roomId.indexOf("single") == -1) {
-
-                MongoClient.connect(url, { useUnifiedTopology: true }, function (err, client) {
-                    if (err) {
-                        io.to(info1.roomId).emit('chatLists', { code: 1, msg: err });
-                    }
-                    var db = client.db("user");
-                    var col = db.collection("chatLists"); //获取到chatLists表
-                    col.find({ "roomId": info1.roomId }).toArray((err, docs) => { //获取到所有的文档
-                        if (err) {
-                            io.to(info1.roomId).emit('chatLists', { code: 1, msg: err });
-                        } else {
-                            io.to(socket.id).emit('oldChat', { code: 0, data: docs });
-
-                            socket.broadcast.to(info1.roomId).emit('inORout', { msg: info1.user + "进入群聊", count: info1.count });//广播给房间内其他人
-                            io.to(info1.roomId).emit('chatLists', {
-                                code: 0, data: [{ "userName": "机器喵", "userChat": "欢迎" + info1.user + "童鞋加入群聊~" }]
-                            });//广播给房间所有人
-                        }
-                        client.close();
-                    })
-                });
-
-
-
-
-
-
-                // socket.join(info.roomId); //添加到房间
-
+            if (info.roomId && info.roomId.indexOf("single") == -1) {
                 if (socket.adapter.rooms[info.roomId]) {
                     info1["count"] = socket.adapter.rooms[info.roomId].length;
                 }
-
                 fn(info1.count);//客户端回调
+                socket.broadcast.to(info1.roomId).emit('inORout', { msg: info1.user + "进入群聊", count: info1.count });//发送给房间内其他人
 
+                // 向数据库请求原始消息
+                MongoClient.connect(url, { useUnifiedTopology: true }, function (err, client) {
+                    if (err) io.to(info1.roomId).emit('chatLists', { code: 1, msg: err });
+                    var db = client.db("user");
+                    var col = db.collection("chatLists");
+                    col.find({ "roomId": info1.roomId }).toArray((err, docs) => {
+                        if (err) {
+                            io.to(info1.roomId).emit('chatLists', { code: 1, msg: err }); //发送给房间所有人
+                        } else {
+                            io.to(socket.id).emit('oldChat', { code: 0, data: docs }); //只发送给请求的那个人
+                        }
 
+                        io.to(info1.roomId).emit('chatLists', {
+                            code: 0, data: [{ "userName": "机器喵", "userChat": "欢迎" + info1.user + "童鞋加入群聊~" }]
+                        });//发送给房间所有人
+                        client.close();
+                    })
+                });
             }
         })
-        socket.on('login', function (info) {
-            userList[info.user] = socket.id; //把用户socketid存到以用户为属性的list对象里
-        })
+
+        /* 进入退出 */
         socket.on('inORout', function (info) {
+            socket.leave(info1.roomId) //退出聊天室
             if (info1.roomId && info1.roomId.indexOf("single") == -1) {
                 if (socket.adapter.rooms[info1.roomId]) {
                     info1["count"] = socket.adapter.rooms[info1.roomId].length;
                 }
                 socket.broadcast.to(info1.roomId).emit('inORout', { msg: info1.user + "退出群聊", count: info1.count - 1 });
             }
-            socket.leave(info1.roomId)
         })
-        socket.on('disconnect', async function () {
-            if (info1.roomId && info1.roomId.indexOf("single") == -1) {
-                if (socket.adapter.rooms[info1.roomId]) {
-                    info1["count"] = socket.adapter.rooms[info1.roomId].length;
-                }
-                socket.broadcast.to(info1.roomId).emit('inORout', { msg: info1.user + "退出群聊", count: info1.count });
-            }
-        })
+        /* 消息监测 */
         socket.on('sendMsg', function (info) {
             var docs = [{
                 userName: info.userName,
                 userChat: info.msg,
                 userChatTime: dateFormat(new Date()),
-                roomId: info1.roomId
+                roomId: info.roomId
             }]
-
+            console.log(info.roomId);
+            console.log(info1.roomId);
+            // 这里是群聊
             if (info1.roomId && info1.roomId.indexOf("single") == -1) {
-                io.to(info1.roomId).emit('chatLists', { code: 0, data: docs });
-                // MongoClient.connect(url, { useUnifiedTopology: true }, function (err, client) {
-                //     if (err) {
-                //         io.to(info.roomId).emit('chatLists', { code: 1, msg: err });
-                //     }
-                //     var db = client.db("user");
-                //     var col = db.collection("chatLists"); //获取到chatLists表
-                //     col.insertOne(docs[0], function (err, result) {
-                //         if (err) {
-                //             io.to(info.roomId).emit('chatLists', { code: 1, msg: err });
-                //         }
-                //         client.close();
-                //     })
-                // });
-            } else {
+                io.to(info1.roomId).emit('chatLists', { code: 0, data: docs }); //发送给房间所有人
 
-                if (!userList[info.toWho]) { //用户不在线
-                    io.to(socket.id).emit('chatLists', { code: 2, data: docs, msg: "当前用户不在线" });
-                } else {                   
+                MongoClient.connect(url, { useUnifiedTopology: true }, function (err, client) {
+                    if (err) io.to(info.roomId).emit('chatLists', { code: 1, msg: err });
+                    var db = client.db("user");
+                    var col = db.collection("chatLists");
+                    col.insertOne(docs[0], function (err, result) {
+                        if (err) io.to(info.roomId).emit('chatLists', { code: 1, msg: err });
+                        client.close();
+                    })
+                });
+            } else { //这是单聊
+
+                if (!userList[info.toWho]) { //用户未登陆，提示
+                    io.to(socket.id).emit('chatLists', { code: 2, data: docs, msg: "当前用户不在线" }); //给请求人发消息
+                } else {
+                    console.log(socket);
+                    console.log(socket.adapter.rooms);
+                    
                     if (!socket.adapter.rooms[info.roomId].sockets[userList[info.toWho]]) { //用户不在当前聊天室
                         io.to(userList[info.toWho]).emit('openNewPage', { roomId: info.roomId, roomName: info.userName, msg: docs });
                     } else {
@@ -114,6 +100,16 @@ exports.socket1 = function (io) {
                 //     code: 0, data:
                 //         [{ "userName": "机器喵", "userChat": msg }]
                 // });
+            }
+        })
+        socket.on('disconnect', async function () {
+            // console.log("用户下线");
+            socket.leave(info1.roomId) //退出聊天室
+            if (info1.roomId && info1.roomId.indexOf("single") == -1) {
+                if (socket.adapter.rooms[info1.roomId]) {
+                    info1["count"] = socket.adapter.rooms[info1.roomId].length;
+                }
+                socket.broadcast.to(info1.roomId).emit('inORout', { msg: info1.user + "退出群聊", count: info1.count });
             }
         })
     });
